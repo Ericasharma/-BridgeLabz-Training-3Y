66e2b72c64ab1fd0d828c560babcab93af840d0e
Erica Sharma <sharmaerica31@gmail.com>
2025-08-28 22:09:02 +0530

    Strings code

--- Level 2/NumberFormatDemo.java
+++ Level 2/NumberFormatDemo.java
@@ -1,31 +1,31 @@
 import java.util.Scanner;
 
-public class NumberPointerDemo {
-   
+public class NumberFormatDemo {
     public static void generateException(String text) {
-       
-        int num = Integer.parseInt(text);
-        System.out.println("Converted number: " + num);
+
+        int number = Integer.parseInt(text);
+        System.out.println("Converted Number: " + number);
     }
 
     public static void handleException(String text) {
         try {
-            int num = Integer.parseInt(text);
-            System.out.println("Converted number: " + num);
+            int number = Integer.parseInt(text);
+            System.out.println("Converted Number: " + number);
         } catch (NumberFormatException e) {
             System.out.println("Caught NumberFormatException: " + e.getMessage());
         } catch (RuntimeException e) {
-            System.out.println("Caught RuntimeException: " + e.getMessage());
+            System.out.println("Caught RuntimeException: " + e);
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter text to convert: ");
+
+        System.out.print("Enter a number: ");
         String text = sc.nextLine();
 
-     
         handleException(text);
+
         sc.close();
     }
 }
